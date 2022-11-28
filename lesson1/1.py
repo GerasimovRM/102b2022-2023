9bdf03e9b5c7afb3eed2a4ee489adbd4f849f6ac
Roman M. Gerasimov <romagrizly@gmail.com>
2022-11-28 15:05:23 +0500

    Initial commit

---
+++ lesson1/1.py
@@ -0,0 +1,22 @@
+a = 7
+b = 3
+c = 1.5e15
+d = a + b
+
+print(a + b, a - b, a * b, a ** b)
+print(a / b, a // b, a % b)
+
+st1 = "Hi"
+st2 = "aBc"
+print(st1 + st2)
+print(st1 * 10)
+print(type(d))
+
+x = True
+y = False
+z = True
+
+w = x or y
+u = x and y
+print(w, u)
+
